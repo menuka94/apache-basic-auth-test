4d5e2774d42b4be393c9bd39e97c8069c21c7f0a
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2019-07-30 15:01:36 +0530

    add workerId, add slf4j logging

--- src/main/java/org/wso2/App.java
+++ src/main/java/org/wso2/App.java
@@ -1,7 +1,5 @@
 package org.wso2;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.http.Header;
 import org.apache.http.HttpRequest;
 import org.apache.http.auth.AuthenticationException;
@@ -11,25 +9,25 @@ import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
-    private static final Log LOG = LogFactory.getLog(App.class);
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
-        WorkerOne workerOne = new WorkerOne();
-        WorkerTwo workerTwo = new WorkerTwo();
-
-        workerOne.start();
-        workerTwo.start();
+        Worker[] workers = new Worker[10];
+        for (int i = 0; i < workers.length; i++) {
+            workers[i] = new Worker("admin", "admin");
+            workers[i].start();
+        }
     }
 
-    public static void generateAuthHeader(String username, String password) throws AuthenticationException {
+    static void generateAuthHeader(int workerId, String username, String password) throws AuthenticationException {
         UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
         BasicScheme basicScheme = new BasicScheme();
 
         HttpRequest httpRequest = new HttpGet("http://localhost:8688");
         HttpContext httpContext = new BasicHttpContext();
-
         Header header = basicScheme.authenticate(credentials, httpRequest, httpContext);
-        LOG.info(header);
+        LOG.info("Worker: {}, {}", workerId, header);
     }
 }
---
+++ src/main/java/org/wso2/Worker.java
@@ -0,0 +1,29 @@
+package org.wso2;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.auth.AuthenticationException;
+
+public class Worker extends Thread {
+    private static int workerId = 0;
+    private static final Log LOG = LogFactory.getLog(Worker.class);
+    private String username;
+    private String password;
+
+    public Worker(String username, String password) {
+        this.username = username;
+        this.password = password;
+        workerId++;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                App.generateAuthHeader(workerId, username, password);
+            }
+        } catch (AuthenticationException e) {
+            LOG.error(e.getStackTrace());
+        }
+    }
+}
