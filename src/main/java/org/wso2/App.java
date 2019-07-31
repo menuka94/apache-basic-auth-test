b3df14b6db67fb2154fce113d0b1f4e2861c3564
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2019-07-31 16:10:47 +0530

    clean up code

--- src/main/java/org/wso2/App.java
+++ src/main/java/org/wso2/App.java
@@ -2,8 +2,17 @@ package org.wso2;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.http.Header;
+import org.apache.http.HttpRequest;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,29 +20,18 @@ import org.slf4j.LoggerFactory;
 public class App {
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-    private static void startWorkers() {
-        Worker[] workers = new Worker[10];
-        for (int i = 0; i < workers.length; i++) {
-            workers[i] = new Worker("admin", "admin");
-            workers[i].start();
-        }
-    }
-
     @GET
+    @Path("{username}/{password}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String sayHello() {
-        return "Hello";
-    }
+    public String generateAuthHeader(@PathParam("username") String username, @PathParam("password") String password)
+            throws AuthenticationException {
+        UsernamePasswordCredentials usernamePasswordCredentials = new UsernamePasswordCredentials(username, password);
+        BasicScheme basicScheme = new BasicScheme();
 
-    // @GET
-    // @Produces(MediaType.TEXT_PLAIN)
-    // public static void generateAuthHeader(int workerId, String username, String password) throws AuthenticationException {
-    //     UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
-    //     BasicScheme basicScheme = new BasicScheme();
-    //
-    //     HttpRequest httpRequest = new HttpGet("http://localhost:8688");
-    //     HttpContext httpContext = new BasicHttpContext();
-    //     Header header = basicScheme.authenticate(credentials, httpRequest, httpContext);
-    //     LOG.info("Worker: {}, {}", workerId, header);
-    // }
+        HttpRequest httpRequest = new HttpGet("http://localhost:8688");
+        HttpContext httpContext = new BasicHttpContext();
+        Header header = basicScheme.authenticate(usernamePasswordCredentials, httpRequest, httpContext);
+        LOG.info(header.toString());
+        return "";
+    }
 }
